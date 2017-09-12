0c72fd207ec653142e431c084bf47f1979a09b78
Andrew Abrazovskiy <andres7480311@mail.ru>
2017-09-12 13:09:59 +0300

    Product/feature: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let line = [], args = [[]], result = [];
+  let one = first.split(''),
+      two = second.split('');
+  let i = 0;
+  two.forEach((top) => {
+    one.forEach((bottom) => {
+      line.push(top * bottom);
+    });
+    args[i] = Array(i).fill(0);
+    args[i].push(...line);
+    line.length = 0;
+    i++;
+  })
+  let sizeX = args.length,
+      sizeY = args[sizeX - 1].length,
+      sumLine = 0;
+  for (let i = sizeY - 1; i >= 0; i--) {
+    for (let j = sizeX - 1; j >= 0; j--) {
+      if (args[j][i] == undefined) {
+        args[j][i] = 0;
+      }
+      sumLine += args[j][i];
+    }
+    result.unshift(sumLine);
+    sumLine = 0;
+  }
+  result.reverse();
+  let solut = result.map((item, i) => {
+    if (i === result.length - 1) return item;
+    if ((item + '').length == 1) {
+      return item;
+    }
+    result[i + 1] += +((item + '').substr(0, (item + '').length - 1));
+    return +((item + '').charAt(((item + '').length - 1)));
+  })
+  return solut.reverse()
+              .join('');
 }
